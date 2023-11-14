3f28d7c2e375d105899c718288e444ba2596ed23
lonpo <dncper@gmail.com>
2023-11-14 02:01:38 +0100

    home3

--- pshare.sql
+++ pshare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3310
--- Généré le : sam. 11 nov. 2023 à 19:38
--- Version du serveur : 10.4.20-MariaDB
--- Version de PHP : 8.0.9
+-- Généré le : mar. 14 nov. 2023 à 02:01
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `deletemessage`
+--
+
+CREATE TABLE `deletemessage` (
+  `id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL,
+  `sender` int(11) NOT NULL,
+  `recever` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `message`
 --
 
@@ -32,68 +45,71 @@ CREATE TABLE `message` (
   `sender` int(11) NOT NULL,
   `contenu` text NOT NULL,
   `date` datetime NOT NULL,
-  `recever` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `recever` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Déchargement des données de la table `message`
 --
 
-INSERT INTO `message` (`num_mes`, `sender`, `contenu`, `date`, `recever`) VALUES
-(1, 1, 'test', '2023-11-07 00:00:00', 2),
-(2, 2, 'test2', '2023-11-08 00:00:00', 1),
-(3, 1, 'un malien americain', '2023-11-01 00:00:00', 3),
-(4, 5, 'Cv fomba', '2023-11-10 07:17:00', 1),
-(5, 1, 'cv bro et toi ', '2023-11-10 00:00:00', 5),
-(6, 5, 'Sinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heincdddcSinon ca fait un bail heindsdsSinon ca fait un bail heindsdsddsSinon ca fait un bail heindsdsdsssssssssssssssssSinon ca fait un bail hein', '2023-11-10 11:41:06', 1),
-(7, 3, 'EEEH guy', '2023-11-10 12:42:08', 5),
-(8, 1, 'test test ', '2023-11-10 15:24:42', 5),
-(9, 1, 'lonpo', '2023-11-10 19:19:18', 5),
-(10, 1, 'guindo', '2023-11-10 19:20:15', 5),
-(11, 1, 'aziz', '2023-11-10 19:20:40', 2),
-(12, 5, 'wai djo', '2023-11-10 19:53:51', 1),
-(13, 1, 'quoi de neuf bro', '2023-11-10 19:56:13', 5),
-(14, 1, 'bon faisons un test djo***', '2023-11-10 19:57:39', 5),
-(15, 5, 'un autre malien americain', '2023-11-10 19:58:06', 1),
-(16, 5, 'test', '2023-11-11 14:43:24', 1),
-(24, 5, 'salut', '2023-11-11 15:23:42', 1),
-(26, 5, 'xxx', '2023-11-11 15:40:15', 1),
-(27, 5, 'ZZZ', '2023-11-11 15:44:08', 1),
-(28, 1, 'et toi bro', '2023-11-11 15:46:26', 5),
-(29, 5, 'wai mon gaz', '2023-11-11 15:46:40', 1),
-(30, 1, 'ca fait un bail hien', '2023-11-11 15:46:52', 5),
-(31, 5, 'depuis quand', '2023-11-11 15:47:03', 1),
-(32, 1, 'je pensais pas trop que ca allait marche mais Dieu merci ca va ', '2023-11-11 15:47:43', 5),
-(33, 5, 'et de ton cote ?', '2023-11-11 15:48:06', 1),
-(34, 1, 'ca va alhamdoullilah', '2023-11-11 15:48:24', 5),
-(35, 5, 'sinon bro les news', '2023-11-11 15:48:57', 1),
-(36, 5, 'et djo', '2023-11-11 15:49:03', 1),
-(37, 1, 'quel News ?', '2023-11-11 15:56:51', 5),
-(38, 1, 'xxxxxxxxxx', '2023-11-11 17:41:53', 5),
-(39, 1, 'xxxxxxx', '2023-11-11 17:42:02', 5),
-(40, 1, 'hehehe', '2023-11-11 17:42:26', 5),
-(41, 5, 'test', '2023-11-11 17:43:57', 1),
-(42, 5, 'c\'etait quoi letest', '2023-11-11 17:46:35', 1),
-(43, 5, 'c\'etait quoi letest', '2023-11-11 17:46:42', 1),
-(44, 5, 'c\'etait quoi letest', '2023-11-11 17:47:15', 1),
-(45, 5, 'un autre truc', '2023-11-11 17:55:21', 1),
-(46, 1, 'et toi aussi', '2023-11-11 17:55:37', 5),
-(47, 1, 'ta dis quoi', '2023-11-11 17:56:07', 5),
-(48, 5, 'desole j\'ai arreter ', '2023-11-11 17:56:43', 1),
-(49, 1, 'test1', '2023-11-11 17:57:06', 5),
-(50, 1, 'test2', '2023-11-11 17:57:10', 5),
-(51, 1, 'test3', '2023-11-11 17:57:14', 5),
-(52, 1, 'test4', '2023-11-11 18:04:12', 5),
-(53, 1, 'test5', '2023-11-11 18:04:22', 5),
-(54, 1, 'test6', '2023-11-11 18:04:27', 5),
-(55, 1, 'un autre malien americain', '2023-11-11 18:06:37', 5),
-(56, 1, 'tu sais de quoi parle ton bro', '2023-11-11 18:07:42', 3),
-(57, 5, 'wai bro', '2023-11-11 18:12:58', 3),
-(58, 5, 'fadiga', '2023-11-11 18:13:31', 1),
-(59, 3, 'djo ca faisait un bail et les affaires ?', '2023-11-11 18:15:25', 5),
-(60, 5, 'alhamdoulliah on remercie le bo Dieu', '2023-11-11 18:15:49', 3),
-(61, 5, 'juste un test', '2023-11-11 18:36:43', 3),
-(62, 3, 'ok le test est bon', '2023-11-11 18:36:53', 5);
+INSERT INTO `message` (`num_mes`, `sender`, `contenu`, `date`, `recever`, `deleted`) VALUES
+(1, 1, 'test', '2023-11-07 00:00:00', 2, 0),
+(2, 2, 'test2', '2023-11-08 00:00:00', 1, 0),
+(3, 1, 'un malien americain', '2023-11-01 00:00:00', 3, 0),
+(4, 5, 'Cv fomba', '2023-11-10 07:17:00', 1, 0),
+(5, 1, 'cv bro et toi ', '2023-11-10 00:00:00', 5, 0),
+(6, 5, 'Sinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heinSinon ca fait un bail heincdddcSinon ca fait un bail heindsdsSinon ca fait un bail heindsdsddsSinon ca fait un bail heindsdsdsssssssssssssssssSinon ca fait un bail hein', '2023-11-10 11:41:06', 1, 0),
+(7, 3, 'EEEH guy', '2023-11-10 12:42:08', 5, 0),
+(8, 1, 'test test ', '2023-11-10 15:24:42', 5, 0),
+(9, 1, 'lonpo', '2023-11-10 19:19:18', 5, 0),
+(10, 1, 'guindo', '2023-11-10 19:20:15', 5, 0),
+(11, 1, 'aziz', '2023-11-10 19:20:40', 2, 0),
+(12, 5, 'wai djo', '2023-11-10 19:53:51', 1, 0),
+(13, 1, 'quoi de neuf bro', '2023-11-10 19:56:13', 5, 0),
+(14, 1, 'bon faisons un test djo***', '2023-11-10 19:57:39', 5, 0),
+(15, 5, 'un autre malien americain', '2023-11-10 19:58:06', 1, 0),
+(16, 5, 'test', '2023-11-11 14:43:24', 1, 0),
+(24, 5, 'salut', '2023-11-11 15:23:42', 1, 0),
+(26, 5, 'xxx', '2023-11-11 15:40:15', 1, 0),
+(27, 5, 'ZZZ', '2023-11-11 15:44:08', 1, 0),
+(28, 1, 'et toi bro', '2023-11-11 15:46:26', 5, 0),
+(29, 5, 'wai mon gaz', '2023-11-11 15:46:40', 1, 0),
+(30, 1, 'ca fait un bail hien', '2023-11-11 15:46:52', 5, 0),
+(31, 5, 'depuis quand', '2023-11-11 15:47:03', 1, 0),
+(32, 1, 'je pensais pas trop que ca allait marche mais Dieu merci ca va ', '2023-11-11 15:47:43', 5, 0),
+(33, 5, 'et de ton cote ?', '2023-11-11 15:48:06', 1, 0),
+(34, 1, 'ca va alhamdoullilah', '2023-11-11 15:48:24', 5, 0),
+(35, 5, 'sinon bro les news', '2023-11-11 15:48:57', 1, 0),
+(36, 5, 'et djo', '2023-11-11 15:49:03', 1, 0),
+(37, 1, 'quel News ?', '2023-11-11 15:56:51', 5, 0),
+(38, 1, 'xxxxxxxxxx', '2023-11-11 17:41:53', 5, 0),
+(39, 1, 'xxxxxxx', '2023-11-11 17:42:02', 5, 0),
+(40, 1, 'hehehe', '2023-11-11 17:42:26', 5, 0),
+(41, 5, 'test', '2023-11-11 17:43:57', 1, 0),
+(46, 1, 'et toi aussi', '2023-11-11 17:55:37', 5, 0),
+(47, 1, 'ta dis quoi', '2023-11-11 17:56:07', 5, 0),
+(49, 1, 'test1', '2023-11-11 17:57:06', 5, 0),
+(50, 1, 'test2', '2023-11-11 17:57:10', 5, 0),
+(51, 1, 'test3', '2023-11-11 17:57:14', 5, 0),
+(52, 1, 'test4', '2023-11-11 18:04:12', 5, 0),
+(53, 1, 'test5', '2023-11-11 18:04:22', 5, 0),
+(56, 1, 'tu sais de quoi parle ton bro', '2023-11-11 18:07:42', 3, 0),
+(57, 5, 'message supprime', '2023-11-11 18:12:58', 3, 1),
+(59, 3, 'djo ca faisait un bail et les affaires ?', '2023-11-11 18:15:25', 5, 0),
+(62, 3, 'message supprime', '2023-11-11 18:36:53', 5, 1),
+(82, 3, 'cv', '2023-11-13 15:19:20', 1, 0),
+(112, 3, 'message supprime', '2023-11-13 21:48:32', 5, 1),
+(114, 5, 'message supprime', '2023-11-13 22:29:21', 3, 1),
+(115, 5, 'gwaka gwaka', '2023-11-13 22:29:27', 3, 0),
+(116, 5, 'cc dramee', '2023-11-13 23:48:46', 3, 0),
+(117, 5, 'cc broooo', '2023-11-14 00:06:34', 3, 0),
+(118, 5, 'et toi', '2023-11-14 00:06:43', 3, 0),
+(119, 5, 'sais pas', '2023-11-14 00:06:55', 3, 0),
+(120, 5, 'cc bro', '2023-11-14 00:09:08', 3, 0),
+(121, 3, 'cc', '2023-11-14 00:09:25', 5, 0),
+(122, 3, 'ca fait un bail', '2023-11-14 00:09:39', 5, 0),
+(123, 5, '', '2023-11-14 00:10:16', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -106,15 +122,7 @@ CREATE TABLE `newmessage` (
   `sender` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   `recever` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `newmessage`
---
-
-INSERT INTO `newmessage` (`id`, `sender`, `message_id`, `recever`) VALUES
-(32, 1, 56, 3),
-(34, 5, 58, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -130,7 +138,7 @@ CREATE TABLE `projet` (
   `statut` enum('actif','clos') DEFAULT NULL,
   `description` text NOT NULL,
   `cahier` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Déchargement des données de la table `projet`
@@ -155,7 +163,7 @@ CREATE TABLE `users` (
   `niveau` enum('Licence','Master','Doctorat') DEFAULT NULL,
   `img` varchar(25) NOT NULL,
   `cv` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Déchargement des données de la table `users`
@@ -170,6 +178,15 @@ INSERT INTO `users` (`num_users`, `nom_complet`, `email_user`, `mdp_user`, `comp
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `deletemessage`
+--
+ALTER TABLE `deletemessage`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tesd` (`message_id`),
+  ADD KEY `tesa` (`recever`),
+  ADD KEY `tesv` (`sender`);
 
 --
 -- Index pour la table `message`
@@ -206,16 +223,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `deletemessage`
+--
+ALTER TABLE `deletemessage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `num_mes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `num_mes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
 
 --
 -- AUTO_INCREMENT pour la table `newmessage`
 --
 ALTER TABLE `newmessage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT pour la table `projet`
@@ -232,6 +255,14 @@ ALTER TABLE `users`
 --
 -- Contraintes pour les tables déchargées
 --
+
+--
+-- Contraintes pour la table `deletemessage`
+--
+ALTER TABLE `deletemessage`
+  ADD CONSTRAINT `tesa` FOREIGN KEY (`recever`) REFERENCES `users` (`num_users`),
+  ADD CONSTRAINT `tesd` FOREIGN KEY (`message_id`) REFERENCES `message` (`num_mes`),
+  ADD CONSTRAINT `tesv` FOREIGN KEY (`sender`) REFERENCES `users` (`num_users`);
 
 --
 -- Contraintes pour la table `message`
